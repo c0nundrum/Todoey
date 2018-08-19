6ec9a089ec5aee3007a45b7b0e2db6d1de7ab81f
Danilo Ribeiro <daniloribeiro@danilos-macbook-air.local>
2018-08-19 18:40:33 -0300

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,17 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Danilo Ribeiro on 19/08/18.
+//  Copyright © 2018 Danilo Ribeiro. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    
+    var done : Bool = false
+    
+}
